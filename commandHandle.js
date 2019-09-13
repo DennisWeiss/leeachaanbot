a3cd7f55de9b0eb59fa1e02b2b9ae6ca0d9b5ac8
Dennis Weiss <dennis.weiss@stud.hszg.de>
2019-09-13 22:28:04 +0200

    Reformatted code

--- commandHandle.js
+++ commandHandle.js
@@ -4,16 +4,16 @@ const conf = require('./conf/conf')
 
 
 function handleCommand(client, target, context, cmd) {
-    switch (cmd) {
-        case '!p':
-            User.findOne({name: context.username})
-                .exec(function (err, user) {
-					const points = user ? user.points : 0
-                    client.say(target, `@${context.username} Du hast ${points} ${points === 1 ? 
-                      conf.currency.nameSingular : conf.currency.namePlural}.`)
-                })
-            break;
-    }
+  switch (cmd) {
+    case '!p':
+      User.findOne({name: context.username})
+        .exec(function (err, user) {
+          const points = user ? user.points : 0
+          client.say(target, `@${context.username} Du hast ${points} ${points === 1 ?
+            conf.currency.nameSingular : conf.currency.namePlural}.`)
+        })
+      break
+  }
 }
 
-module.exports = {handleCommand};
+module.exports = {handleCommand}
--- fetchCycle.js
+++ fetchCycle.js
@@ -7,36 +7,40 @@ const User = require('./model/User')
 
 const userExcluded = username => [conf.broadcasterChannelName, conf.botUsername, ...conf.excludedUsers].includes(username)
 
-const updateViewerPoints = function(viewer) {
+const updateViewerPoints = function (viewer) {
   if (!userExcluded(viewer)) {
     User.findOne({'name': viewer})
       .exec(function (err, user) {
         if (user) {
-          User.findOneAndUpdate({'name': viewer}, {name: user.name, points: user.points +
-              conf.currency.pointsPerViewIteration})
-            .exec(function (err, user) {})
+          User.findOneAndUpdate({'name': viewer}, {
+            name: user.name, points: user.points +
+              conf.currency.pointsPerViewIteration
+          })
+            .exec(function (err, user) {
+            })
         } else {
           const user = new User({name: viewer, points: conf.currency.pointsPerViewIteration})
-          user.save().then(() => {})
+          user.save().then(() => {
+          })
         }
       })
   }
 }
 
-const update = function() {
-    axios.get(`https://api.twitch.tv/helix/streams?user_login=${conf.broadcasterChannelName}`, {
-        headers: {
-            Authorization: `OAuth ${global.accessToken}`,
-            'Client-ID': conf.clientId
-        }
+const update = function () {
+  axios.get(`https://api.twitch.tv/helix/streams?user_login=${conf.broadcasterChannelName}`, {
+    headers: {
+      Authorization: `OAuth ${global.accessToken}`,
+      'Client-ID': conf.clientId
+    }
+  })
+    .then(res => {
+      if (res.data && res.data.data && res.data.data.length > 0) {
+        axios.get(`http://tmi.twitch.tv/group/user/${conf.broadcasterChannelName}/chatters`)
+          .then(res => res.data && res.data.chatters &&
+            [...res.data.chatters.viewers, ...res.data.chatters.moderators].forEach(updateViewerPoints))
+      }
     })
-        .then(res => {
-            if (res.data && res.data.data && res.data.data.length > 0) {
-                axios.get(`http://tmi.twitch.tv/group/user/${conf.broadcasterChannelName}/chatters`)
-                    .then(res => res.data && res.data.chatters &&
-                        [...res.data.chatters.viewers, ...res.data.chatters.moderators].forEach(updateViewerPoints))
-            }
-        })
 
 }
 

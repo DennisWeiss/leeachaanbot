e6842f10da46004ec913751e97847a42c5492fb3
Dennis Weiss <dennis.weiss@stud.hszg.de>
2019-09-13 22:26:06 +0200

    Added config option to exclude users

--- fetchCycle.js
+++ fetchCycle.js
@@ -5,18 +5,22 @@ const conf = require('./conf/conf')
 const User = require('./model/User')
 
 
+const userExcluded = username => [conf.broadcasterChannelName, conf.botUsername, ...conf.excludedUsers].includes(username)
+
 const updateViewerPoints = function(viewer) {
+  if (!userExcluded(viewer)) {
     User.findOne({'name': viewer})
-        .exec(function (err, user) {
-            if (user) {
-                User.findOneAndUpdate({'name': viewer}, {name: user.name, points: user.points +
-                    conf.currency.pointsPerViewIteration})
-                    .exec(function (err, user) {})
-            } else {
-                const user = new User({name: viewer, points: conf.currency.pointsPerViewIteration})
-                user.save().then(() => {})
-            }
-        })
+      .exec(function (err, user) {
+        if (user) {
+          User.findOneAndUpdate({'name': viewer}, {name: user.name, points: user.points +
+              conf.currency.pointsPerViewIteration})
+            .exec(function (err, user) {})
+        } else {
+          const user = new User({name: viewer, points: conf.currency.pointsPerViewIteration})
+          user.save().then(() => {})
+        }
+      })
+  }
 }
 
 const update = function() {
@@ -27,7 +31,7 @@ const update = function() {
         }
     })
         .then(res => {
-            if (res.data.data.length > 0) {
+            if (res.data && res.data.data && res.data.data.length > 0) {
                 axios.get(`http://tmi.twitch.tv/group/user/${conf.broadcasterChannelName}/chatters`)
                     .then(res => res.data && res.data.chatters &&
                         [...res.data.chatters.viewers, ...res.data.chatters.moderators].forEach(updateViewerPoints))

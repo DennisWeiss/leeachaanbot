40e6dd4ff22ee9c6d13f6925cdab993d3474a898
Dennis Weiss <dennis.weiss@stud.hszg.de>
2019-09-13 22:37:59 +0200

    Added help command

--- commandHandle.js
+++ commandHandle.js
@@ -3,15 +3,33 @@ const User = require('./model/User')
 const conf = require('./conf/conf')
 
 
+function help(target, username) {
+  client.say(target, `@${username} Verfügbare Kommandos:\n\n
+  !p, !dangos    Sagt dir wie viele Dangos du besitzt.`)
+}
+
+function dangos(traget, username) {
+  User.findOne({name: username})
+    .exec(function (err, user) {
+      const points = user ? user.points : 0
+      client.say(target, `@${username} Du hast ${points} ${points === 1 ?
+        conf.currency.nameSingular : conf.currency.namePlural}.`)
+    })
+}
+
 function handleCommand(client, target, context, cmd) {
   switch (cmd) {
     case '!p':
-      User.findOne({name: context.username})
-        .exec(function (err, user) {
-          const points = user ? user.points : 0
-          client.say(target, `@${context.username} Du hast ${points} ${points === 1 ?
-            conf.currency.nameSingular : conf.currency.namePlural}.`)
-        })
+      dangos(target, context.username)
+      break
+    case '!dangos':
+      dangos(target, context.username)
+      break
+    case '!h':
+      help(target, context.username)
+      break
+    case '!help':
+      help(target, context.username)
       break
   }
 }

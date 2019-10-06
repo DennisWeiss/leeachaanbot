9ed492b107e2f7f8fa26703906c6efdacdc3a772
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-10-06 15:15:47 +0200

    Added docs folder for GitHub Pages

---
+++ docs/precache-manifest.c965fcf9d86f7dabfe4c76db1cc28136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6c7b9b9e661ffee5ce7668b43047d3",
+    "url": "/leeachaanbot/index.html"
+  },
+  {
+    "revision": "bbaa046b1a864235109a",
+    "url": "/leeachaanbot/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "79933e50d50fcb1dab8d",
+    "url": "/leeachaanbot/static/css/main.24289d23.chunk.css"
+  },
+  {
+    "revision": "bbaa046b1a864235109a",
+    "url": "/leeachaanbot/static/js/2.02d218ce.chunk.js"
+  },
+  {
+    "revision": "79933e50d50fcb1dab8d",
+    "url": "/leeachaanbot/static/js/main.003b539b.chunk.js"
+  },
+  {
+    "revision": "9ca6709646541bdc14bf",
+    "url": "/leeachaanbot/static/js/runtime-main.739f54b5.js"
+  }
+]);

712d708a49617b2f9ffe50238f2d98ac0f768fee
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-10-27 23:04:18 +0100

    Logging followers set size now

--- chatbot/src/fetchCycle.js
+++ chatbot/src/fetchCycle.js
@@ -71,6 +71,7 @@ const update = function () {
                 .then(followersList => {
                   const followers = new Set(followersList)
                   console.log(followers)
+                  console.log(followers.size)
                   axios.get(`http://tmi.twitch.tv/group/user/${conf.broadcasterChannelName}/chatters`)
                     .then(res => res.data && res.data.chatters &&
                       [...res.data.chatters.viewers,

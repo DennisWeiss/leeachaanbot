1141dfb28068b87474ab3ab247268064a8a3f86b
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-12-04 20:24:29 +0100

    Adding displayName to user model again

--- chatbot/src/model/User.js
+++ chatbot/src/model/User.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const User = mongoose.model('User', {
   userId: String,
   name: String,
+  displayName: String,
   profilePictureUrl: String,
   points: Number
 })

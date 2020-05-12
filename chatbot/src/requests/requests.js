ebd62ae1cd8d9b07deb99d4ca535e38376bc6e28
Dennis Weiss <weiss.dennis98@yahoo.de>
2020-05-12 23:22:01 +0200

    Sending app access token when fetching user info

--- chatbot/src/requests/requests.js
+++ chatbot/src/requests/requests.js
@@ -1,14 +1,19 @@
 const axios = require('axios')
 const Config = require('../model/Config')
+const Security = require('../model/Security')
 
 let config = null
+let security = null
+
 
 Config.findOne({}).exec((err, _config) => config = _config)
+Security.findOne({}).exec((err, _security) => security = _security)
 
 
 const fetchUserById = userId => axios.get(`https://api.twitch.tv/helix/users?id=${userId}`, {
   headers: {
-    'Client-ID': config.clientId
+    'Client-ID': config.clientId,
+    Authorization: `Bearer ${security.appAccessToken}`
   }
 })
 
